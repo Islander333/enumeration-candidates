9fdc911f4bb383614b79ea8a4f86efba7c0bb5a0
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-10-11 18:42:41 -0700

    add multiple methods to filters.rb, with test code in main.rb

--- filters.rb
+++ filters.rb
@@ -2,15 +2,56 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+    #loop through the array
+    @candidates.each do |candidate|
+      return candidate if candidate[:id] == id
+    end
+    nil
   end
   
   def experienced?(candidate)
-    # Your code Here
+    #takes in a single candidate(hash) note: not the array
+    candidate[:years_of_experience] >= 2
+    #returns true if the candidate has 2 years of experience
+    #returns false otherwise
+    #this is a method that gives you back data, it should not output anything
+  end
+
+  #helper method for github points
+  def github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  #helper method for knows ruby or python
+  def knows_ruby_or_python(candidate)
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  end
+
+  #helper method for applied within last 15 days
+  def applied_recently?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+
+  #helper method for ages over 17
+  def of_age?(candidate)
+    candidate[:age] >= 18
+  end
+
+  def qualified?(candidate)
+    experienced?(candidate) &&
+    github_points?(candidate) &&
+    knows_ruby_or_python(candidate) &&
+    applied_recently?(candidate) &&
+    of_age?(candidate)
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    #takes in a collection of candidates
+    #returns a subset of the candidates that meet the criteria
+   candidates.select { |candidate| qualified?(candidate) }
   end
   
-  # More methods will go below
+  #ordered_by_qualifications method
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
--- main.rb
+++ main.rb
@@ -7,3 +7,18 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+#tests for the experienced? method
+pp experienced?(@candidates[0]) #expected true
+pp experienced?(@candidates[1]) #expected false
+
+#tests for the find method
+pp find(5)
+pp find(10)
+pp find (20) #should return nil
+
+#tests for qualified_candidates
+pp qualified_candidates(@candidates)
+
+#tests for the ordered_by_qualifications
+pp ordered_by_qualifications(@candidates)
